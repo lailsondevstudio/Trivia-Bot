fdcd80cf86f35739e0343e69ed39e35b1f71fdbf
Lailson Developer <lailsoncristiano.ph@gmail.com>
2026-02-26 22:20:33 -0800

    TriviaBot
    this is one little bot i write in JavaScript feel free to use...!

---
+++ TriviaBot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Trivia";
+let botLocation = "Your Mind";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming languase is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + "Javascript";
+
+codingFact = favoriteLanguage + "its one of the most popular programming languages.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "can be used for both frontend and backend development.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
